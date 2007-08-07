ec64737cc91712b0dd3df7f40a094c586f9a4d13
superartstudio <superartstudio@2b15753a-6236-0410-a473-71e0dff46810>
2007-08-07 17:16:52 +0000

    eye renderer under construction

--- trunk/temp/element/eye_round/render.php
+++ trunk/temp/element/eye_round/render.php
@@ -1,5 +1,14 @@
 <?php
 
+function render_eye_round($obj)
+{
+	global $v;
 
+	$x = calculate_pos(obj, 'eye_round', 'pos_x');
+	$y = calculate_pos(obj, 'eye_round', 'pos_y');
+	$r = calculate_size(obj, 'eye_round', 'size');
+
+	imagefilledellipse($v['image_scene'], );
+}
 
 ?>
