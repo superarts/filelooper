2bd094669615d6b370d3bce542db274722b4dac0
superartstudio <superartstudio@2b15753a-6236-0410-a473-71e0dff46810>
2007-08-09 16:08:35 +0000

    working on renderer

--- temp/element/eye_round/render.php
+++ temp/element/eye_round/render.php
@@ -1,12 +1,17 @@
 <?php
 
+/*
+ * obj: 'char_liuyue'
+ *
+ */
+
 function render_eye_round($obj)
 {
 	global $v;
 
-	$x = calculate_pos(obj, 'eye_round', 'pos_x');
-	$y = calculate_pos(obj, 'eye_round', 'pos_y');
-	$r = calculate_size(obj, 'eye_round', 'size');
+	$x = calculate_pos($obj, 'eye_round', 'pos_x');
+	$y = calculate_pos($obj, 'eye_round', 'pos_y');
+	$r = calculate_size($obj, 'eye_round', 'size');
 
 	imagefilledellipse($v['image_scene'], );
 }
