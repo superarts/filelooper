6cf9508cdec0432f25cafeef3bb0fb6aaf512a8a
superartstudio <superartstudio@2b15753a-6236-0410-a473-71e0dff46810>
2007-08-28 16:07:21 +0000

    fixed some warning info

--- trunk/temp/element/eye_round/profile.php
+++ trunk/temp/element/eye_round/profile.php
@@ -12,12 +12,16 @@
  */
 
 $v['eye_round'] = array(
-	'pos_x'			=> 20,	//	percentage of face width
+	'pos_x'			=> 35,	//	percentage of face width
 	'pos_y'			=> 50,
 	'size'			=> 30,		
+    'ball_x'        => 5,   //  ball pos
+    'ball_y'        => 5,
 	'ball_size'		=> 10,
 	'color'			=> $v['color_white'],
 	'ball_color'	=> $v['color_black'],
+    'x_left'        => 1,
+    'x_right'       => -1,
 	'name'			=> 'Round Eyes');
 
 ?>
