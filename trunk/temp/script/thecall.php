6720343dcf71d97a46ae55932970c2a3fca7ecb5
superartstudio <superartstudio@2b15753a-6236-0410-a473-71e0dff46810>
2007-09-25 17:09:42 +0000

    start working for short show 'the call'

---
+++ trunk/temp/script/thecall.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * An Empty Scene
+ *
+$v['scene_000'] = array(
+	'duration'	=> ,
+	'bg_color'	=> $v['color_blue'],
+	'camera'	=> array(
+		),
+	'object'	=> array(
+		),
+	'script'	=> array(
+		),
+	'name'		=> 'Scene ');
+ *
+ */
+
+$v['scene_00001'] = array(
+	'duration'	=> 10,
+	'bg_color'	=> $v['color_blue'],
+	'camera'	=> array(
+		),
+	'object'	=> array(
+		),
+	'script'	=> array(
+		),
+	'name'		=> 'Scene ');
+
+?>
