63acb9afba306b6a452a2a1a74d93e45fa88c5b6
superartstudio <superartstudio@2b15753a-6236-0410-a473-71e0dff46810>
2007-09-27 13:27:25 +0000

    working on the call

--- trunk/temp/object/taiyang/profile.php
+++ trunk/temp/object/taiyang/profile.php
@@ -1,3 +1,26 @@
 <?php
 
+$v['room_taiyang'] = array(
+	'type'		=> 'misc',
+	'pos_x'		=> 0.5,
+	'pos_y'		=> -0.2,
+	'scale'		=> 1.2,
+	'item_x'	=> 0,
+	'item_y'	=> 0,
+	'item'		=> 'room/taiyang_living_room',
+	'event'		=> array(),
+	'name'		=> 'Living Room of Taiyang');
+
+$v['photo_taiyang_fanghuayuan'] = array(
+	'type'		=> 'misc',
+	'pos_x'		=> 0.9,
+	'pos_y'		=> 0.8,
+	'scale'		=> 0.1,
+	'item_x'	=> 0,
+	'item_y'	=> 0,
+	'item'		=> 'photo/taiyang_fanghuayuan',
+	'event'		=> array(),
+	'name'		=> 'Living Room of Taiyang');
+
+
 ?>
