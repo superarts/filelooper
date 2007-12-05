ae432aa6f09bb84d575a8c4e6c36c454d8d90fc5
superartstudio <superartstudio@2b15753a-6236-0410-a473-71e0dff46810>
2007-12-05 03:14:00 +0000

    added files for the one man show

---
+++ temp/object/oneman/profile.php
@@ -0,0 +1,25 @@
+<?php
+
+$v['room_oneman_hall'] = array(
+	'type'		=> 'misc',
+	'pos_x'		=> 0.5,
+	'pos_y'		=> -0.2,
+	'scale'		=> 1.2,
+	'item_x'	=> 0,
+	'item_y'	=> 0,
+	'item'		=> 'room/oneman_hall',
+	'event'		=> array(),
+	'name'		=> 'Hall of the One Man Show');
+
+$v['item_oneman_light'] = array(
+	'type'		=> 'misc',
+	'pos_x'		=> 0.5,
+	'pos_y'		=> -0.2,
+	'scale'		=> 1.2,
+	'item_x'	=> 0,
+	'item_y'	=> 0,
+	'item'		=> 'item/oneman_light',
+	'event'		=> array(),
+	'name'		=> 'Light from the slide player of the One Man Show');
+
+?>
