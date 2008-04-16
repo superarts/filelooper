b6f971e0dcc8f8680b244b429e79371c2414e8c7
superartstudio <superartstudio@2b15753a-6236-0410-a473-71e0dff46810>
2008-04-16 15:52:52 +0000

    working on toolkit subtitle maker

---
+++ temp/toolkit/subtitle_maker/run_counter.php
@@ -0,0 +1,41 @@
+<?php
+
+function print_backspace($count = 1)
+{
+	for ($i = 0; $i < $count; $i++)
+	{
+		echo "\010";
+	}
+
+	return $i;
+}
+
+$scale = 10;	//	delay 1/x seconds
+if (isset($argv[1]))
+	$start = $argv[1];
+else
+	$start = 0;
+
+$counter = $start * 10;
+$length = 10;
+
+if (false)
+{
+	echo "/*
+		Hit <Enter> when you think it's time to make formatted output.
+		Copy the code below from console to a php file.
+		TODO add ');' in the end of the code:
+	 */\n\n";
+	echo "\$name = array(\n";
+}
+
+while (true)
+{
+	$index = str_pad($counter, $length, '0', STR_PAD_LEFT);
+	echo "\t$index, " . 
+	usleep(1000000 / $scale);
+	print_backspace($length + 3);
+	$counter++;
+}
+
+?>
